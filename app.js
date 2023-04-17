618c61ca46af6594a5428cbc4bc06926a5224422
Jeffrey Seneff <jeffsnff@gmail.com>
2023-04-17 09:36:30 -0400

    challenge completed

---
+++ app.js
@@ -0,0 +1,39 @@
+
+
+function howManyLetters(){
+
+  // Removes the old result from page
+  const oldResult = document.getElementById("result");
+  while (oldResult.hasChildNodes()) {
+    oldResult.removeChild(oldResult.firstChild);
+  }
+
+  // Grabs user input from page and forces it to lower case
+  let userInput = document.getElementById('userInput').value.toLowerCase()
+
+  // letters we will be testing against
+  const alphabet = 'abcdefghijklmnopqurstuvwxyz'
+
+  //creats the object 
+  let letterCount = {}
+
+  // iterates user input over the alapabet string
+  // then adds the letter in user input into the object
+  for (let i = 0; i<userInput.length; i++){
+    if (alphabet.includes(userInput[i])){
+      if ( letterCount[userInput[i]]){
+        letterCount[userInput[i]]++
+      }else{
+        letterCount[userInput[i]] = 1
+      }
+    }
+  }
+
+  // creates a list item then appends to result ul
+  for(const [key, value ] of Object.entries(letterCount)){
+    listItem = document.createElement('li')
+    listItem.innerHTML =  key + ' : ' + ' ' + value
+    document.getElementById('result').appendChild(listItem)
+  }  
+}
+
